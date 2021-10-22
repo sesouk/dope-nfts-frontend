fbfd976834241c9dd8113b0ac8fa27cee856daa7
Kevin Tran <sesouk@gmail.com>
2021-10-22 16:37:10 -0600

    able to mint nfts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers"
+import myDopeNft from './utils/MyDopeNFT.json'
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
@@ -9,12 +11,77 @@ const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
 
 const App = () => {
+  const [currentAccount, setCurrentAccount] = useState("")
+
   // Render Methods
-  const renderNotConnectedContainer = () => (
-    <button className="cta-button connect-wallet-button">
-      Connect to Wallet
-    </button>
-  );
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window
+
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      return
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+
+    const accounts = await ethereum.request({ method: 'eth_accounts' })
+
+    if (accounts.length !== 0) {
+      const account = accounts[0]
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account)
+    } else {
+      console.log("No authorized account found");
+    }
+  }
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window
+
+      if (!ethereum) {
+        alert("Get MetaMask!")
+        return
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" })
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0])
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const askContractToMint = async () => {
+    const CONTRACT_ADDRESS = "0x8E2Ee3C768c90FaF2003DaB0545d3C07b4d78Bbd"
+
+    try {
+      const { ethereum } = window
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myDopeNft.abi, signer);
+
+        console.log("Opening wallet to pay gass...");
+        let nftTxn = await connectedContract.makeNFT()
+
+        console.log("Mining... please wait");
+        await nftTxn.wait()
+
+        console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
+
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected()
+  }, [])
 
   return (
     <div className="App">
@@ -24,7 +91,15 @@ const App = () => {
           <p className="sub-text">
             Each unique. Each beautiful. Discover your NFT today.
           </p>
-          {renderNotConnectedContainer()}
+          {currentAccount === "" ? (
+            <button onClick={connectWallet} className="cta-button connect-wallet-button">
+              Connect to Wallet
+            </button>
+          ) : (
+            <button onClick={askContractToMint} className="cta-button connect-wallet-button">
+              Mint NFT
+            </button>
+          )}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
